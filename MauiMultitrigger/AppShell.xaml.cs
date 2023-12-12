10b953499567e0a2cb3a27e420232de4ccc37f4d
Håvard Lerstad <haavard@lerstad.org>
2023-12-12 21:38:17 +0100

    Initial project load

---
+++ MauiMultitrigger/AppShell.xaml.cs
@@ -0,0 +1,7 @@
+﻿namespace MauiMultitrigger {
+    public partial class AppShell : Shell {
+        public AppShell() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiMultitrigger/MainPage.xaml.cs
@@ -0,0 +1,7 @@
+﻿namespace MauiMultitrigger {
+    public partial class MainPage : ContentPage {
+        public MainPage() {
+            InitializeComponent();
+        }
+    }
+}
